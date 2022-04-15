617329d27acac16a0c23a5ebbe5db0b7f0e80669
Elif İrem Kara <83959541+elfrmkr@users.noreply.github.com>
2022-04-15 09:20:33 +0300

    Time: 104 ms (73.28%), Space: 49.3 MB (71.82%) - LeetHub

--- 344-reverse-string/344-reverse-string.js
+++ 344-reverse-string/344-reverse-string.js
@@ -2,6 +2,10 @@
  * @param {character[]} s
  * @return {void} Do not return anything, modify s in-place instead.
  */
+ 
+/*
+-- my initial solution from previous time
+
 var reverseString = function(s) {
         
     for( let i = 0; i < s.length/2 ; i++) {
@@ -9,6 +13,28 @@ var reverseString = function(s) {
         let firsts = s[i]
         s[i] = s[s.length - 1 -i]
         s[s.length - 1 -i] = firsts
+    }
+    
+};
+
+*/ 
+
+
+var reverseString = function(s) {
+    
+    reverse(s, 0, s.length -1)
+};
+
+
+var reverse = function(s, start, end) {
+    
+    while(start < end) {
+        
+        let temp = s[start]
+        s[start] = s[end]
+        s[end] = temp
+        start++
+        end--
     }
     
 };
