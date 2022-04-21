ae87948ecf7866d993805757dac95767a6460afc
Elif İrem Kara <83959541+elfrmkr@users.noreply.github.com>
2022-04-21 10:13:16 +0300

    Time: 72 ms (74.17%), Space: 44.3 MB (45.68%) - LeetHub

---
+++ 206-reverse-linked-list/206-reverse-linked-list.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+      if (!head || !head.next) {
+        return head;
+      }
+      let first = head;
+      let second = first.next;
+  
+      while(second) {
+        const temp = second.next;
+        second.next = first;
+        first = second;
+        second = temp;
+      }
+  
+      head.next = null;
+      head = first;
+    return head
+};
